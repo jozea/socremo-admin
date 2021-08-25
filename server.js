b45c95abf5160b0716c4ee16fbcb3e5c1dee8973
Joseph <joseph@rubikpay.net>
2021-08-25 09:52:37 +0100

    third update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/socremo-admin'));
+app.use(express.static(__dirname + './socremo-admin/dist/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile('index.html', {root:'dist/socremo-admin/'});
+res.sendFile('./socremo-admin/dist//index.html');
 });
 
 // Start the app by listening on the default Heroku port
