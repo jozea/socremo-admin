1d42936de9f4f1a744bd03de5568cc9c314d5150
Joseph <joseph@rubikpay.net>
2021-08-25 14:46:28 +0100

    socremo update

--- server.js
+++ server.js
@@ -5,15 +5,34 @@
 // const app = express();
 
 // // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/socremo-admin/'));
+// app.use(express.static(__dirname + './dist/socremo-admin/'));
 
 // app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/socremo-admin/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/socremo-admin/index.html'));
 // });
 
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
+
+
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + './socremo-admin/dist/'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile('./socremo-admin/dist//index.html');
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
 
 
 //Install express server
@@ -23,11 +42,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './socremo-admin/dist/'));
+app.use(express.static('./dist/socremo-admin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile('./socremo-admin/dist//index.html');
+res.sendFile('index.html', {root: '/dist/socremo-admin/'});
 });
 
 // Start the app by listening on the default Heroku port
